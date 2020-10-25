a88f5d7811fe3d5f70caa6ffe8879ffced4b1673
Martin Kubica <martinko29.km@gmail.com>
2020-10-25 19:05:22 +0100

    +

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
 
 namespace Open_Lab_04._05
 {
@@ -6,7 +8,15 @@ namespace Open_Lab_04._05
     {
         public string Repeat(string orig, int n)
         {
-            throw new NotImplementedException();
-        }
+            string koniec = "";
+            for (int i = 0; i <orig.Length ; i++)
+            {
+                for (int u = 0; u < n; u++)
+                {
+                    koniec = koniec + orig[i];
+                }
+            }
+            return koniec;
+        }   
     }
 }
